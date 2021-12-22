6a2db65d2f2f16a719ba9ec6f6c91d55527c6322
AhmedMAlAhmed <96461500+AhmedMAlAhmed@users.noreply.github.com>
2021-12-22 10:00:17 +0300

    Fix navigation bar

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 
 class Nav extends StatelessWidget {
@@ -7,41 +9,38 @@ class Nav extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: BottomNavigationBar(
-    items: const <BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: 'setting',
-    backgroundColor: Colors.blue
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(Icons.bar_chart),
-    label: 'Report',
-    backgroundColor: Colors.blue
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(Icons.home),
-    label: 'Home',
-    backgroundColor: Colors.blue
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(Icons.camera),
-    label: 'Camera',
-    backgroundColor: Colors.blue
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(Icons.menu),
-    label: 'Menu',
-    backgroundColor: Colors.blue
-    ),
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+          backgroundColor: Colors.blue
+      ),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.bar_chart),
+          label: 'Report',
+          backgroundColor: Colors.blue
+      ),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.blue
+      ),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.camera),
+          label: 'Camera',
+          backgroundColor: Colors.blue
+      ),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.menu),
+          label: 'Menu',
+          backgroundColor: Colors.blue
+
+      ),
+
     ],
-    ),
+
+   ),
     );
   }
-//       void _onItemTapped(int index) {
-// setState(() {
-// _selectedIndex = index;
-// });
-// }
-
-
 }
