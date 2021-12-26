2d00d0a777f967ccf3a3c6e4b3d110c83f6d3775
Goldenknight65 <61232032+Goldenknight65@users.noreply.github.com>
2021-12-26 08:34:51 +0300

    add bold text

---
+++ lib/boldText.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Boldtext extends StatelessWidget {
+  const Boldtext({ Key? key }) : super(key: key);
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
